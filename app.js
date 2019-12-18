e07d901abff7caa30983fb874e8679b094879fbe
josephakeni <josephakeni@gmail.com>
2019-12-18 23:13:52 +0000

    Added Routes

--- app.js
+++ app.js
@@ -1,28 +1,64 @@
 const express = require('express');
-const path = require('path')
+const http = require('http')
+const path = require('path');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+
+// Mine
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 // Init App
 const app = express();
 
-// Load View Engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({extended: false}));
 
-// set public folder
-app.use(express.static(path.join(__dirname, 'public')));
-// Home Route
-app.get('/', function(req, res){
-    res.render('index', {
-        title:'Bookings'
-    });
+// Add Middleware
+app.use('/admin', adminRoutes);
+
+app.use(shopRoutes);
+
+app.use((req, res, next)=>{
+    res.status(404).sendFile(path.join(__dirname, 'views',  '404.html'))
 });
 
-// Add Route
-app.get('/bookings/add', function(req, res){
-    res.render('add_booking', {
-        title:'Add Booking'
-    });
-});
+// const url = "mongodb://localhost/jotonialogistics";
+
+// // MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) {
+// //   if (err) throw err;
+// //   console.log("Database created!");
+// //   db.close();
+// // });
+
+// var mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/jotonialogistics', {useNewUrlParser: true}, );
+
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   // we're connected!
+//   console.log("Database created!");
+// });
+
+// // Load View Engine
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
+
+// // set public folder
+// app.use(express.static(path.join(__dirname, 'public')));
+// // Home Route
+// app.get('/', function(req, res){
+//     res.render('index', {
+//         title:'Bookings'
+//     });
+// });
+
+// // Add Route
+// app.get('/bookings/add', function(req, res){
+//     res.render('add_booking', {
+//         title:'Add Booking'
+//     });
+// });
 // Start Server
-app.listen(3000,function(){
+app.listen(3000, ()=>{
     console.log('Server started on port 3000 ===>');
 });
