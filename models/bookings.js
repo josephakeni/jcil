f957464cf8dc412fbccb98ea8eecc01c3313cd69
Joseph Akeni <joseph.akeni@essdocs.com>
2019-12-31 20:38:25 +0000

    Added static files

--- models/bookings.js
+++ models/bookings.js
@@ -1,20 +1,20 @@
-// let mongoose = require('mongoose');
+let mongoose = require('mongoose');
 
-// //Bookings schema
-// let bookingSchema = mongoose.Schema({
-//     title:{
-//         type: String,
-//         required: true
-//     },
-//     author:{
-//         type: String,
-//         required: true
-//     },
-//     body:{
-//         type: String,
-//         required: true
-//     }
+//Bookings schema
+let bookingSchema = mongoose.Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    author:{
+        type: String,
+        required: true
+    },
+    body:{
+        type: String,
+        required: true
+    }
 
-// });
+});
 
-// let Bookings = module.exports = mongoose.model('Bookings', bookingSchema);
+let Bookings = module.exports = mongoose.model('Bookings', bookingSchema);
